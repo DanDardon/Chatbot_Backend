0a7a1eedc9a149d3488d9fae6284121493b5c28d
DanDardon <98858688+DanDardon@users.noreply.github.com>
2025-05-23 22:21:23 -0600

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,19 @@
-import cx_Oracle
+import oracledb
 from backend.config import DB_USER, DB_PASS, DB_DSN
 
 def get_connection():
-    
     try:
-        conn = cx_Oracle.connect(user=DB_USER, password=DB_PASS, dsn=DB_DSN)
+        # Modo "thin" automático: sin necesidad de Oracle Client
+        conn = oracledb.connect(
+            user=DB_USER,
+            password=DB_PASS,
+            dsn=DB_DSN
+        )
         print("✅ Conexión exitosa a Oracle")
         return conn
-    except cx_Oracle.DatabaseError as e:
-        error, = e.args
-        print("❌ Error al conectar con Oracle:", error.message)
+    except oracledb.Error as e:
+        print("❌ Error al conectar con Oracle:", str(e))
         return None
-        
-
 
 if __name__ == "__main__":
     get_connection()
