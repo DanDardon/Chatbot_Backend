5a05cee3364bfb3d85b4df99253d7b350e7a1b00
DanDardon <98858688+DanDardon@users.noreply.github.com>
2025-05-23 22:52:53 -0600

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,3 @@
 DB_USER = "ADMIN"
 DB_PASS = "Rd30072003!!"
-DB_DSN = "adb.us-ashburn-1.oraclecloud.com:1521/geeb562edc24945_agentemedico_high.adb.oraclecloud.com"
+DB_DSN = "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.us-ashburn-1.oraclecloud.com))(connect_data=(service_name=geeb562edc24945_agentemedico_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
